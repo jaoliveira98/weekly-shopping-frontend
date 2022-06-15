616fe1c8de13bb91d035d7189d71e85da17e9831
Sofia Chaves <41347104+SofiaChaves@users.noreply.github.com>
2022-06-16 00:26:03 +0100

    Hello Message, Inter font integration (#8)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <>
-      Hello world
-    </>
-  );
+    return (
+        <div className="text-3xl font-extrabold text-slate-800 flex flex-col items-center py-16">
+            Hello Sofia
+        </div>
+    );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        fontFamily: {
+            inter: ['Inter', 'system-ui'],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
