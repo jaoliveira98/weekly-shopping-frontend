292583edddbe23644f4012d250c451205a5092c0
Joao Oliveira <86269259+jaoliveira98@users.noreply.github.com>
2022-06-20 20:05:35 +0100

    Formating Meals List

--- src/pages/meals/MealsPage.js
+++ src/pages/meals/MealsPage.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 export function MealsPage() {
   const [meals, setMeals] = useState([]);
+
   useEffect(() => {
     axios
       .get("https://localhost:5000/api/v1/meal")
@@ -18,27 +19,30 @@ export function MealsPage() {
   }, []);
 
   return (
-    <>
-      <div className="flex justify-between p-8">
+    <div className="p-8 ">
+      <div className="flex justify-between mb-6">
         <div>
           <h1 className="text-3xl font-extrabold text-slate-800">My meals</h1>
           <p>Add and edit your cookbook</p>
         </div>
         <Link to="create">+</Link>
       </div>
+      {meals.length === 0 && "Nothing found"}
 
-      <div>
-        <p>
-          {meals.map((meal) => {
-            return (
-              <>
-                Meal name:
-                {meal.name} <br />
-              </>
-            );
-          })}
-        </p>
+      <div className="grid grid-cols-2 gap-4">
+        {meals.map((meal) => {
+          return (
+            <div className="w-full">
+              <img
+                src="https://static.onecms.io/wp-content/uploads/sites/44/2019/08/26231113/5783153.jpg"
+                alt="meal"
+                className="w-full h-40 object-cover rounded-lg mb-2"
+              />
+              <h3>{meal.name}</h3>
+            </div>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 }
